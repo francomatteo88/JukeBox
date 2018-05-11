f48285fdf06d756159abb205bf2ce0b206bb36b0
fliptonic-account <cloud@fliptonic.com>
2018-05-11 18:00:56 +0200

    first commit

---
+++ songs.js
@@ -0,0 +1,133 @@
+var json = [{
+		"title": "fischi",
+		"image": "./images/avicii-stories.jpg",
+		"audio": "registrazione.m4a",
+		"album": "Stories",
+		"author": "Avicii",
+		"category": "Dance",
+		"year": 2000
+	},
+	{
+		"title": "cPrayer in C 2012",
+		"image": "./images/robinshulz-prayerinc.jpg",
+		"audio": "am.mp3",
+		"album": "Prayer in C",
+		"author": "Robin Schulz",
+		"category": "Dance",
+		"year": 2012
+	},
+	{
+		"title": "aThese Days 2008",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "These Days",
+		"author": "Rudimental",
+		"category": "Dance",
+		"year": 2008
+	},
+	{
+		"title": "bJohnny Be Good 1999",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 1999
+	},
+	{
+		"title": "dJohnny Be Good",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 2018
+	},{
+		"title": "bWaiting For Love 2000",
+		"image": "./images/avicii-stories.jpg",
+		"audio": "am.mp3",
+		"album": "Stories",
+		"author": "Avicii",
+		"category": "Dance",
+		"year": 2000
+	},
+	{
+		"title": "cPrayer in C 2012",
+		"image": "./images/robinshulz-prayerinc.jpg",
+		"audio": "am.mp3",
+		"album": "Prayer in C",
+		"author": "Robin Schulz",
+		"category": "Dance",
+		"year": 2012
+	},
+	{
+		"title": "aThese Days 2008",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "These Days",
+		"author": "Rudimental",
+		"category": "Dance",
+		"year": 2008
+	},
+	{
+		"title": "bJohnny Be Good 1999",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 1999
+	},
+	{
+		"title": "dJohnny Be Good",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 2018
+	},{
+		"title": "bWaiting For Love 2000",
+		"image": "./images/avicii-stories.jpg",
+		"audio": "am.mp3",
+		"album": "Stories",
+		"author": "Avicii",
+		"category": "Dance",
+		"year": 2000
+	},
+	{
+		"title": "cPrayer in C 2012",
+		"image": "./images/robinshulz-prayerinc.jpg",
+		"audio": "am.mp3",
+		"album": "Prayer in C",
+		"author": "Robin Schulz",
+		"category": "Dance",
+		"year": 2012
+	},
+	{
+		"title": "aThese Days 2008",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "These Days",
+		"author": "Rudimental",
+		"category": "Dance",
+		"year": 2008
+	},
+	{
+		"title": "bJohnny Be Good 1999",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 1999
+	},
+	{
+		"title": "dJohnny Be Good",
+		"image": "./images/These-Days-cover-rudimental.jpg",
+		"audio": "am.mp3",
+		"album": "Johnny Be Good",
+		"author": "Chuck Berry",
+		"category": "RockERoll",
+		"year": 2018
+	}];
